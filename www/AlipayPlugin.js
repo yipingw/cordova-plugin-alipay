bd32a4e117f10d53a754bd3db2b6515f5feda264
Yiping Wang <yipingw@outlook.com>
2015-10-16 22:15:18 +0800

    还原之前的逻辑

--- www/AlipayPlugin.js
+++ www/AlipayPlugin.js
@@ -3,7 +3,7 @@ var exec = require('cordova/exec');
 
 var Alipay = function() {};
 
-Alipay.prototype.alipay = function(out_trade_no, subject, bodtxt, total_fee, successCallback, errorCallback, callbackUrl) {
+Alipay.prototype.alipay = function(payInfo, successCallback, errorCallback) {
     if (errorCallback == null) {
         errorCallback = function() {}
     }
@@ -18,59 +18,15 @@ Alipay.prototype.alipay = function(out_trade_no, subject, bodtxt, total_fee, suc
         return;
     }
 
-    if (!callbackUrl) {
+    if (!payInfo) {
         errorCallback('参数错误！');
         return;
     }
     //{"out_trade_no":out_trade_no, "subject": subject,"bodtxt":bodtxt, "total_fee": total_fee, "callbackUrl": callbackUrl}
-    exec(successCallback, errorCallback, 'AlipayPlugin', 'pay', [out_trade_no, subject, bodtxt, total_fee, callbackUrl]);
+    exec(successCallback, errorCallback, 'AlipayPlugin', 'pay', [payInfo]);
 };
 
 var alipay = new Alipay();
 module.exports = alipay;
 
-
-
-/***************************使用demo****************************/
-//cordova.plugins.AlipayPlugin.alipay();
-
-function testAlipay() {
-    var subject = "测试的商品";
-    var body = "该测试商品的详细描述";
-    var price = "0.01";
-    var out_trade_no = getOutTradeNo();
-    var callbackUrl = "http://host:port/path/Alipay/notify_url.aspx";
-    var successCallback = function(msg) {
-        console.log("successCallback");
-        if(typeof msg == "object"){
-            console.log("success--->" + msg.memo);
-            console.log("success--->" + msg.result);
-            console.log("success--->" + msg.resultStatus);
-            alert("success--->" + msg.resultStatus + " : "+msg.result + " : " + msg.memo);
-        }else{
-            alert("success--->" + msg);
-            
-        }
-        
-    };
-
-
-    /**
-     * 错误代码：
-     * 9000  订单支付成功
-     * 8000  正在处理中
-     * 4000  订单支付失败
-     * 6001  用户中途取消
-     * 6002  网络连接出错
-     */
-    var errorCallback = function(msg) {
-        console.log("errorCallback");
-        alert("error--->" + msg);
-    };
-    cordova.plugins.AlipayPlugin.alipay(out_trade_no, subject, body, price, successCallback, errorCallback, callbackUrl);
-}
-
-
-function getOutTradeNo() {
-    return "082215222612710";
-}
+});
