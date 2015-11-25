8f18fb32735e830b5a7f2a349a76b2fb10ea6c25
Yiping Wang <yipingw@outlook.com>
2015-11-25 15:23:51 +0800

    修改了支付宝插件的实现

--- src/ios/AlipayPlugin.m
+++ src/ios/AlipayPlugin.m
@@ -7,13 +7,15 @@
 
 @implementation AlipayPlugin
 
+NSString *appScheme;
+
 - (void)pay:(CDVInvokedUrlCommand*)command
 {
-    
-    
     //应用注册scheme,在AlixPayDemo-Info.plist定义URL types
-    NSString *appScheme = @"mankr";
-
+  NSString *path = [[NSBundle mainBundle] bundlePath];
+  NSString *finalPath = [path stringByAppendingPathComponent:@"Info.plist"];
+  NSMutableDictionary* plistDictionary = [NSMutableDictionary dictionaryWithContentsOfFile:finalPath];
+  appScheme = [[[plistDictionary objectForKey:@"CFBundleURLTypes"] firstObject] objectForKey:@"CFBundleURLSchemes"][0];
     
     //将签名成功字符串格式化为订单字符串,请严格按照该格式
     NSString *orderString = [command.arguments objectAtIndex:0];
@@ -27,5 +29,16 @@
         }];
         
     }
+}
+
+- (void)handleOpenURL:(NSNotification *)notification {
+  if (appScheme == nil) return;
+  
+  NSURL *url = [notification object];
+  if ([url isKindOfClass:[NSURL class]] && [url.absoluteString hasPrefix:[appScheme stringByAppendingString:@"://safepay/"]]) {
+    [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
+      
+    }];
+  }
 }
 @end
